bc536208ec32acf157a6835a21d3da8a324ed382
Javid Askerov <askerovlab@gmail.com>
2018-09-15 15:48:42 +0400

    update the app after changes

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
   mode: 'development',
+  watch: true,
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.[contenthash].js'
   },
+  plugins: [
+    new CleanWebpackPlugin(['build'], {
+      exclude: ['template.html']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Testing React Lessons',
+      template: './build/template.html'
+    })
+  ],
   module: {
     rules: [
       {
