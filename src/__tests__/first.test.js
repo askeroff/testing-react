6d59302ff57b109e2b7632d20a2842106f4cbc97
Javid Askerov <askerovlab@gmail.com>
2018-09-18 14:13:48 +0400

    first test from first lessons

--- src/__tests__/first.test.js
+++ src/__tests__/first.test.js
@@ -1,5 +1,47 @@
-describe('See our first test', () => {
-  test('Some description', () => {
-    expect(1).toBe(1);
+function sum(a, b) {
+  return a + b;
+}
+
+function nameMe(name) {
+  return { name };
+}
+
+function division(a, b) {
+  if (b === 0) {
+    throw new Error('You can not divide by zero');
+  }
+  return a / b;
+}
+
+test('Another example test', () => {
+  // Boolean(value)
+  expect(sum(0, 0)).toBeFalsy();
+  expect(sum(1, 2)).toBeTruthy();
+  /*
+    false
+    0
+    ""
+    NaN
+    undefined
+    null
+  */
+});
+
+test('Division test', () => {
+  const result = division(10, 5);
+  expect(division.bind(null, 10, 0)).toThrowError('You can not divide');
+  expect(result).toBe(2);
+});
+
+test('Should return an object with a passed name', () => {
+  const result = nameMe('John');
+  expect(result).toEqual({
+    name: 'John'
   });
+});
+
+test('Example test', () => {
+  const result = sum(1, 3);
+  expect(result).not.toBe(2);
+  expect(result > 1).toBe(true);
 });
