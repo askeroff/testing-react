08861dbbed47d285af9e299ea4ac2efc45ed0c66
Javid Askerov <askerovlab@gmail.com>
2018-09-18 14:13:40 +0400

    import add form

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-
+import AddForm from './add-form';
 class App extends React.Component {
   render() {
-    return <div> Hello, world! </div>;
+    return (
+      <div>
+        <div style={{ width: '600px', margin: 'auto' }}>
+          <h2>Books to read</h2>
+          <AddForm />
+        </div>
+      </div>
+    );
   }
 }
 
