458f8d9c443a6626997c367afc7876d64683d3c9
Javid Askerov <askerovlab@gmail.com>
2018-09-18 21:30:07 +0400

    lesson-5

--- src/__tests__/add-form.test.jsx
+++ src/__tests__/add-form.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { fireEvent, render } from 'react-testing-library';
 import AddForm from '../add-form';
 
 describe('AddForm adds new books', () => {
@@ -7,6 +7,18 @@ describe('AddForm adds new books', () => {
   test('Component renders', () => {
     component = render(<AddForm />);
   });
+
+  test('User can enter a new book name', () => {
+    fireEvent.change(component.getByLabelText('Add new book'), {
+      target: {
+        value: 'War and Peace'
+      }
+    });
+    expect(component.getByLabelText('Add new book').value).toBe(
+      'War and Peace'
+    );
+  });
+
   afterAll(() => {
     component.unmount();
   });
--- src/add-form.jsx
+++ src/add-form.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class AddForm extends React.Component {
+  state = {
+    bookName: ''
+  };
+  handleChange = e => {
+    this.setState({
+      bookName: e.target.value
+    });
+  };
   render() {
+    const { bookName } = this.state;
     return (
       <form>
-        <label style={{ paddingRight: '10px' }}>Add new book</label>
-        <input type="text" />
+        <label htmlFor="bookname" style={{ paddingRight: '10px' }}>
+          Add new book
+        </label>
+        <input
+          onChange={this.handleChange}
+          value={bookName}
+          id="bookname"
+          type="text"
+        />
         <input type="submit" />
       </form>
     );
