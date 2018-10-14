6911e4de234f7f4e61f9f84b3ef9a797d3aff370
Javid Askerov <askerovlab@gmail.com>
2018-10-14 21:57:11 +0400

    tests lesson

---
+++ src/__tests__/mocks.test.jsx
@@ -0,0 +1,28 @@
+import DataManager from '../data-manager';
+
+function sum(x, y) {
+  return x + y;
+}
+
+jest.mock('../data-manager', () => {
+  return class {
+    getBooks() {
+      return [{ name: 'Book 1', read: false }];
+    }
+  };
+});
+
+describe('Mock tests', () => {
+  test('2 + 3 return five', () => {
+    const sum = jest.fn((x, y) => x + y);
+    expect(sum(2, 3)).toBe(5);
+    expect(sum).toHaveBeenCalled();
+    expect(sum).toHaveBeenCalledTimes(1);
+    expect(sum).toHaveBeenCalledWith(2, 3);
+  });
+
+  test('Datamanager', () => {
+    const manager = new DataManager();
+    console.log(manager.getBooks());
+  });
+});
