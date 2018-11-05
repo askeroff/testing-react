499c45bb428cf7e23c221e1e14782b13d9d3d524
Javid Askerov <askerovlab@gmail.com>
2018-11-05 14:53:24 +0400

    accept delete prop

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -17,19 +17,31 @@ class ListItem extends React.Component {
 
   render() {
     const { checked } = this.state;
-    const { id } = this.props;
+    const { id, deleteBook } = this.props;
     const decorated = checked ? 'line-through' : 'unset';
-    const key = `book-${id}`;
     const checkboxId = `checkbox-${id}`;
+    const buttonId = `button-${id}`;
     return (
-      <li style={{ textDecoration: decorated }} data-testid={key}>
-        <label htmlFor={checkboxId}>
+      <li
+        style={{ textDecoration: decorated, marginBottom: '10px' }}
+        data-testid={'book'}
+      >
+        <label data-testid={checkboxId} htmlFor={checkboxId}>
           <Checkbox
             id={checkboxId}
             onChange={e => this.handleChange(e, id)}
             checked={checked}
           />
           {this.props.name}
+        </label>
+        <label
+          data-testid={buttonId}
+          style={{ marginLeft: '10px' }}
+          htmlFor={buttonId}
+        >
+          <button id={buttonId} onClick={() => deleteBook(id)}>
+            X
+          </button>
         </label>
       </li>
     );
